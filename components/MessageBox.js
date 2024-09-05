5b528e66379037f15dea925bf5c558bcfacb3d1a
Paras <paras@MacBook-Pro.local>
2024-09-05 16:36:09 +0530

    added suspended cron

--- components/MessageBox.js
+++ components/MessageBox.js
@@ -2,7 +2,7 @@ const MessageBox = ({ msg, keyVal , msgClass}) => {
   return (
     <div
       key={keyVal}
-      className={`w-[80%] h-full justify-center text-sm border-[1.5px] border-pink-400  text-white rounded-md bg-pink-300 py-4 px-5 italic ${msgClass}`}
+      className={`w-[80%] h-full justify-center text-sm border-[1.5px] border-pink-400  font-bold text-white rounded-md bg-pink-300 py-4 px-5 italic ${msgClass}`}
     >
       {msg}
     </div>
